4c8875175762374a8c3fd646e91cedfc93e4f7d5
Asicalug <isaac.poissinger@gmail.com>
2023-07-04 17:17:07 +0200

    Updated code

--- bot.py
+++ bot.py
@@ -2,8 +2,9 @@ import discord
 import importlib
 import inspect
 import os
+from itertools import cycle
 
-from discord.ext import commands
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
 from utils.settings import Settings
@@ -27,9 +28,22 @@ for filename in os.listdir("./cogs"):
         bot.load_extension(f"cogs.{filename[:-3]}")
 
 @bot.event
+async def on_message(message):
+    eta = 'eta'
+    if message.content.lower()=="eta":
+        await message.reply("There is currently no ETA. Please read <#1120369654653788302>")
+
+status = cycle(['Watching The Server'])
+
+@tasks.loop(seconds=30)
+async def status_swap():
+    await bot.change_presence(activity=discord.Game(next(status)))
+
+@bot.event
 async def on_ready():
     print(f"{bot.user} is ready")
     print("Loading Views")
+    status_swap.start()
     for filename in os.listdir("./views"):
         if filename.endswith(".py"):
             classes = [obj for name, obj in inspect.getmembers(importlib.import_module(f"views.{filename[:-3]}")) if inspect.isclass(obj) and issubclass(obj, discord.ui.View)]
--- cogs/StaffCommands.py
+++ cogs/StaffCommands.py
@@ -1,7 +1,7 @@
 import discord
 
 from discord.ext import commands
-from discord.commands import SlashCommandGroup, Option
+from discord.commands import SlashCommandGroup, Option, SlashCommand
 
 from views.NewSuggestionView import CreateSuggestion
 from views.NewTicketView import CreateTicket
@@ -12,7 +12,9 @@ class StaffCommands(commands.Cog):
         print(f"{self.__class__.__name__} loaded")
 
     setup = SlashCommandGroup(name="setup", description="Various Setup Commands")
+    embed = SlashCommandGroup(name="embed", description="Various Embed Commands")
     
+
     @setup.command(name="suggestions", description="Sets up Suggestions")
     async def setup_suggestions(
         self,
@@ -59,6 +61,20 @@ class StaffCommands(commands.Cog):
         embed.add_field(name="Ticket Category", value=ticket.mention)
         await ctx.respond(embed=embed)
 
+    @embed.command(name="rules", description="Sends Rules Embed")
+    async def embedrules(
+        self,
+        ctx: discord.ApplicationContext,
+    ):
+        embed1 = discord.Embed(title="Klair", description="Welcome to the Official Discord Server of Klair Client. We are pleased to have you in our journey :D. Before continuing please read the rules and obey them, any user who breaks the rules is going to face some consequences (that is, ban, mute). NOTE: This Discord Server's Layout is inspired by Hybris.", color=discord.Color.red())
+        embed1.set_footer(icon_url="https://asicalug.netlify.app/storage/klair.png",)
+        embed2 = discord.Embed(url="https://discord.gg/Hybris", title="Hybris' Discord", color=discord.Color.red())
+        embed3 = discord.Embed(title="", color=discord.Color.red())
+        embed3.set_image(url="https://media.discordapp.net/attachments/1120373785967738880/1121014281727639684/Klair_Rules.png?width=1040&height=585")
+        embed4 = discord.Embed(description="Check out the Rules before Starting Your Journey In our Discord Server - Klair", title="", color=discord.Color.red())
+        embed5 = discord.Embed(title="", description="Remember, the rules are applied to all the behaviour on the server including Moderators and Staffs. If you See anyone breaking the Rules, report it to any online Staff/Mod.", color=discord.Color.red())
+        embed5.add_field(name="Rules", value="* 1. Be Respectful and dont be mean to others :D\n\n* 2. No Spamming\n\n* 3. No Advertising,\n\n* 4. No Threatening\n\n* 5. Dont share any personal information\n\n* 6. Be a good person :D")
+        await ctx.send_response(embeds=(embed1, embed2, embed3, embed4, embed5))
 
         
     
